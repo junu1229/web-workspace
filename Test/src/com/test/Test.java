ccaeda975eda840f03a9ca10ed3e10b99cce0a55
junu1229 <junu1229@gmail.com>
2023-08-08 19:02:13 +0900

    코테 연습용

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -1,14 +1,21 @@
 package com.test;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Test {
 	
 	public static void main(String[] args) {
 		int[] num_list = {12, 4, 15, 46, 38, -2, 15};
 		int answer = 0;
-		Arrays.stream(num_list).filter(n -> n < 0);
-		Arrays.asList(num_list).indexOf(num_list);
+		for(int i = 0; i<num_list.length; i++) {
+			if (num_list[i]<0) {
+				answer = i;
+			}
+		}
+		
+		System.out.println(IntStream.range(0, num_list.length).filter(i -> num_list[i] < 0).findFirst().orElse(-1));
+		
     }
 }
 
