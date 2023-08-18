4dab2e76745fc810dd8f86a85ebce504e7c806e1
junu1229 <junu1229@gmail.com>
2023-08-18 18:10:50 +0900

    Update Test.java

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -1,10 +1,23 @@
 package com.test;
 
+
 public class Test {
 	
 	public static void main(String[] args) {
-		String myString = "abstract algebra";
-		myString.toLowerCase().replaceAll("a", "A");
+		int[] num_list = {2, 1, 6};
+		int[] answer = new int[num_list.length];
+		int n = 1;
+		System.out.println(num_list.length);
+		for(int i = 0; i<num_list.length; i++) {
+			if(num_list.length-(n+i)>0) {
+				answer[i] = num_list[i+n];
+			} else {
+				answer[i] = num_list[i-n];
+			}
+		}
+		for(int a:answer) {
+			System.out.println(a);
+		}
 	}
 	
 
